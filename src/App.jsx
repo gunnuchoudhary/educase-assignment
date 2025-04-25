e767a83c1086f19ce848616572fc3f83cc98297b
Gunjan <gunjan19feb99@gmail.com>
2025-04-25 15:04:31 +0530

    hosting

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage'
 import Setting from './pages/setting'
 import Signup from './pages/signup'
 import Login from './pages/login'
-function App() {
 
+function App() {
   return (
-    <>
- <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage/>} />
-          <Route path="/settings" element={<Setting/>}/>
-          <Route path="/signup" element={<Signup />} />
-          <Route path='/login' element={<Login />}/>
-        </Routes>
-      </Router>    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/settings" element={<Setting />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
